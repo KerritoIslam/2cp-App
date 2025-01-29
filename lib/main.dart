dfaccef6022801ac0ae6bf7aa93f48fcd6a293c7
KerritoIslam <mi.amour@esi-sba.dz>
2025-01-29 13:30:15 +0100

    theme 1.2

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '2CP App',
-      theme: them.lightTheme,
+      theme: theme.lightTheme,
       home: WelcomePage(),
     );
   }
--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-class them {
+class theme {
     static ThemeData lightTheme = ThemeData(
+      scaffoldBackgroundColor: Color(0xFFFFFFFF),
         primaryColor: Color(0xFF92E3A9),
         textTheme: TextTheme(
             titleSmall: TextStyle(
@@ -36,5 +37,44 @@ class them {
                 fontWeight: FontWeight.w400,
             ),
         )
+
     );
+  static ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: Color(0xFF323030),
+    primaryColor: Color(0xFF92E3A9),
+    textTheme: TextTheme(
+      titleSmall: TextStyle(
+        fontSize: 16,
+        color: Color(0xFF92E3A9),
+        fontWeight: FontWeight.w700,
+
+      ),
+      titleMedium: TextStyle(
+        fontSize: 24,
+        color: Color(0xFF92E3A9),
+        fontWeight: FontWeight.w700,
+      ),
+      displaySmall: TextStyle(
+        fontSize: 16,
+        color: Color(0xFFFFFFFF),
+        fontWeight: FontWeight.w700,
+      ),
+      displayMedium: TextStyle(
+        fontSize: 24,
+        color: Color(0xFFFFFFFF),
+        fontWeight: FontWeight.w700,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 15,
+        color: Color(0xFFFFFFFF),
+        fontWeight: FontWeight.w500,
+      ),
+      bodySmall: TextStyle(
+        fontSize: 12,
+        color: Color(0xFFFFFFFF),
+        fontWeight: FontWeight.w400,
+      ),
+    )
+
+  );
 }
