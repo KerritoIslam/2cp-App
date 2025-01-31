739abd2c5728ca83ac9d485533ee14c8b1bff8eb
KerritoIslam <mi.amour@esi-sba.dz>
2025-01-31 15:38:40 +0100

    Eiter fix

--- lib/features/autentication/data/remots/rest_auth_remote.dart
+++ lib/features/autentication/data/remots/rest_auth_remote.dart
@@ -1,40 +1,40 @@
 import 'package:app/core/dioservices/dio.dart';
+import 'package:app/core/failure/failure.dart';
+import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
 class RestAuthRemote {
   final Dio _dio = DioServices.dio;
 
-  Future<Response> login(String email, String password) async {
+  Future<Either<Failure, Response>> login(String email, String password) async {
     try {
       final response = await _dio.post(
-        '/Auth/Login',
+        '/Auth/Login/',
         data: {
           'email': email,
           'password': password,
         },
       );
-      return response;
+      return right(response);
     } catch (e) {
-      rethrow;
+      return left(Failure(e.toString()));
     }
   }
 
-  Future<Response> register(String name, String email, String password) async {
+  Future<Either<Failure, Response>> register(
+      String name, String email, String password) async {
     try {
       final response = await _dio.post('/Auth/Signup/', data: {
-        "name": name,
         "password": password,
         "email": email,
-        "type": "Company",
-        "company": {"category": "EC"}
       });
-      return response;
+      return right(response);
     } catch (e) {
-      rethrow;
+      return left(Failure(e.toString()));
     }
   }
 
-  Future<Response> logout(String token) async {
+  Future<Either<Failure, Response>> logout(String token) async {
     try {
       final response = await _dio.post(
         '/logout',
@@ -44,13 +44,13 @@ class RestAuthRemote {
           },
         ),
       );
-      return response;
+      return right(response);
     } catch (e) {
-      rethrow;
+      return left(Failure(e.toString()));
     }
   }
 
-  Future<Response> getUserProfile(String token) async {
+  Future<Either<Failure, Response>> getUserProfile(String token) async {
     try {
       final response = await _dio.get(
         '/profile',
@@ -60,13 +60,14 @@ class RestAuthRemote {
           },
         ),
       );
-      return response;
+      return right(response);
     } catch (e) {
-      rethrow;
+      return left(Failure(e.toString()));
     }
   }
 
-  Future<Response> UpdateUser(String token, Map<String, dynamic> data) async {
+  Future<Either<Failure, Response>> updateUser(
+      String token, Map<String, dynamic> data) async {
     try {
       final response = await _dio.put(
         '/edit_user',
@@ -77,21 +78,26 @@ class RestAuthRemote {
           },
         ),
       );
-      return response;
+      return right(response);
     } catch (e) {
-      rethrow;
+      return left(Failure(e.toString()));
     }
   }
 }
+  void main() async {
+    final remot = RestAuthRemote();
+    final response1 =
+        await remot.register('islam', 'islamtestinglogin@gmail.com', '123456');
+    if (response1.isRight()) {
+      print(
+          'this is a register response : ${response1.leftMap((l) => l.toString())}');
+    } else {
+      print(response1);
+    }
 
-void main() async {
-  final remot = RestAuthRemote();
-  final Response response1 =
-      await remot.register('islam', 'islamtestinglogin@gmail.com', '123456');
-  print(
-      '${'this is a login response : ' + response1.data}${response1.statusCode}');
-  final Response response =
-      await remot.login('islamtestinglogin@gmail.com', '123456');
-  print(
-      '${'this is a login response : ' + response.data}${response.statusCode}');
-}
+    final response = await remot.login('islamtestinglogin@gmail.com', '123456');
+    if (response.isRight()) {
+      print('this is a login response : ' + '$response');
+    }
+  }
+
